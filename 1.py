20a9d43839048229d84b4661e00f5ae1c8b4ab03
walter <sallar.ebadi@gmail.com>
2021-03-08 04:04:29 +0330

    2nd

--- 1.py
+++ 1.py
@@ -1,1 +1,8 @@
-print ("kkkk") 
+from datetime import datetime
+odds =[1,3,5,7,9,11]
+right_this_minute=datetime.today().minute
+
+if right_this_minute in odds:
+    print("odd minute")
+else:
+    print("not an odd")
