c7fa230f5837efdc394dfc36b09db66e6b867530
walter <sallar.ebadi@gmail.com>
2021-03-10 01:47:14 +0330

    4th

--- 1.py
+++ 1.py
@@ -9,15 +9,16 @@ import random
 
 odds =[1,3,5,7,9,11]
 
-right_this_minute=datetime.today().minute
 # time_now = datetime.today()
 # right_this_time = time_now.minute
 
 for i in range (8):
-    rnd = random.randrange(0,5)
+    right_this_minute=datetime.today().minute
+    #for calculating time in each loop
+    rnd = random.randrange(0, 20)
     if right_this_minute in odds:
-        print("odd minute")
+        print("the minute is {} => odd minute".format(right_this_minute))
     else:
-        print("not an odd")
-    print(rnd)    
+        print("the minute is {} =>not an odd".format(right_this_minute))
+    print("<<<wait time is {}>>> ".format(rnd))    
     time.sleep(rnd)
