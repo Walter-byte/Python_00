562c835e7526fa0fbe1e2aa74dbdfb9fd3d47c18
walter <sallar.ebadi@gmail.com>
2021-03-11 03:02:26 +0330

    10th

---
+++ 4_1_2.py
@@ -0,0 +1,15 @@
+w = input ("write a word for searching vowels... ")
+v = ["a", "e", "i", "o", "u"]
+found = {}
+found["a"] = 0
+found["e"] = 0
+found["i"] = 0
+found["o"] = 0
+found["u"] = 0
+
+for letter in w:
+    if letter in v:
+        found [letter] += 1
+
+for k,v in found.items():
+    print(k, "was found", v, "time(s).")
